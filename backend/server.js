5738977fb46ca82965da2aacc58a9b219e9202cd
Lucas <l.lukas122@gmail.com>
2026-09-20 19:14:00 -0300

    Corrige conexão do banco MySQL

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ app.post('/denuncias', (req, res) => {
 });
 
 app.get('/denuncias', (req, res) => {
-    const sql = 'SELECT * FROM denuncias ORDER BY data_criacao DESC';
+    const sql = 'SELECT * FROM denuncias ';
 
     db.query(sql, (err, results) => {
         if (err) {
