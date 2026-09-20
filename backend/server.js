e840f55afcee7a7bda2499a58de4b979cd9f2301
Lucas <l.lukas122@gmail.com>
2026-09-20 17:54:05 -0300

    Corrige conexão do banco MySQL

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,9 @@ const db = mysql.createPool({
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
 });
 
 db.getConnection((err, connection) => {
