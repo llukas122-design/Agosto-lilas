98a4bc8093e5dc3290600a03c275142b4d138dc0
Lucas <l.lukas122@gmail.com>
2026-09-20 18:26:35 -0300

    Corrige conexão do banco MySQL

--- backend/server.js
+++ backend/server.js
@@ -88,6 +88,7 @@ app.get('/denuncias', (req, res) => {
 // ------------------------
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor backend rodando na porta ${PORT}`);
 });
